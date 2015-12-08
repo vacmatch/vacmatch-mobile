cd33106a4933cbdc2cebcac7521dfb6cf2e2d445
Pablo Castro <castrinho8@gmail.com>
2015-12-08 21:13:26 +0100

    Add feature to create and add elements in right menu - Add MenuStore with actual elements in menu - Add and empty list of elements in ReportList for testing

--- src/app/components/layout.jsx
+++ src/app/components/layout.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import Reflux from 'reflux'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 import mui from 'material-ui'
+import { History } from 'react-router'
+import MenuStore from '../stores/MenuStore'
 
 // Components
 let AppBar = mui.AppBar
 let Avatar = mui.Avatar
 let LeftNav = mui.LeftNav
+let IconMenu = mui.IconMenu
+let MenuItem = require('material-ui/lib/menus/menu-item')
+let IconButton = mui.IconButton
 
 // Needed for onTouchTap
 // Can go away when react 1.0 release
@@ -17,6 +23,11 @@ injectTapEventPlugin()
  * Layout component
  */
 let Layout = React.createClass({
+  mixins: [
+    Reflux.connect(MenuStore, 'menu'),
+    History
+  ],
+
   propTypes: {
     children: React.PropTypes.element.isRequired
   },
@@ -25,11 +36,36 @@ let Layout = React.createClass({
     this.refs.leftNav.toggle()
   },
 
+  _handleClickRightMenu: function (element) {
+    // If element has url, go to the link
+    if (element.url) {
+      this.history.pushState(null, element.url)
+    }
+    // If element has a callback just use it
+    if (element.callback) {
+      element.callback()
+    }
+  },
+
   render: function () {
+    let rightMenuElements = (
+      this.state.menu.rightMenu.map((element, index) => {
+        return <MenuItem key={index} index={index} primaryText={element.text} onClick={this._handleClickRightMenu.bind(null, element)}/>
+      })
+    )
     return <div>
       <AppBar
         title='VACmatch'
-        iconClassNameRight='muidocs-icon-navigation-expand-more' onLeftIconButtonTouchTap={this._handleTouchTap}/>
+        onLeftIconButtonTouchTap={this._handleTouchTap}
+        iconElementRight={
+          <IconMenu
+            iconButtonElement={
+              <IconButton><i className='material-icons'>more_vert</i></IconButton>
+            }>
+            {rightMenuElements}
+          </IconMenu>
+        }
+        />
       <LeftNav ref='leftNav' docked={false} menuItems={[]}
         header={
             <Avatar src='assets/img/logos/vacmatch.png' size={100} />
