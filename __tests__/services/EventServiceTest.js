11fae882dc6b8d39d6271387ca9d43610dd4ce03
Pablo Castro <pablocastro@vacmatch.com>
2016-03-02 02:26:51 +0100

    Add Event Service tests - Create ControlEvent - Delete Event

--- __tests__/services/EventServiceTest.js
+++ __tests__/services/EventServiceTest.js
@@ -1,23 +1,26 @@
 jest.dontMock('../../src/app/services/EventService')
 
+// Services
+let eventService = require('../../src/app/services/EventService')
+let genericService = require('../../src/app/services/GenericService')
+let reportService = require('../../src/app/services/ReportService')
+let personService = require('../../src/app/services/PersonService')
+let teamService = require('../../src/app/services/TeamService')
+
+let EventElements = require('../../src/app/models/event/Event')
+let Person = require('../../src/app/models/person/Person')
+let Team = require('../../src/app/models/team/Team')
+let Report = require('../../src/app/models/report/Report')
+let InstanceNotFoundException = require('../../src/app/models/exception/InstanceNotFoundException')
+
+// Default elements
+let defaultPerson = null
+let defaultTeam = null
+let defaultReport = null
+let defaultEvent = null
+let defaultControlEvent = null
+
 describe('create Sport Event', function () {
-  // Default elements
-  let eventService = require('../../src/app/services/EventService')
-  let genericService = require('../../src/app/services/GenericService')
-  let reportService = require('../../src/app/services/ReportService')
-  let personService = require('../../src/app/services/PersonService')
-  let teamService = require('../../src/app/services/TeamService')
-
-  let EventElements = require('../../src/app/models/event/Event')
-  let Person = require('../../src/app/models/person/Person')
-  let Team = require('../../src/app/models/team/Team')
-  let Report = require('../../src/app/models/report/Report')
-  let InstanceNotFoundException = require('../../src/app/models/exception/InstanceNotFoundException')
-
-  let defaultPerson = null
-  let defaultTeam = null
-  let defaultReport = null
-  let defaultEvent = null
 
   beforeEach(function () {
     defaultPerson = new Person('person', '', '', '', '', false, false, '', '', '')
@@ -185,4 +188,207 @@ describe('create Sport Event', function () {
         // Check if create was called in GenericService
         expect(genericService.create).not.toHaveBeenCalled()
   })
+})
+
+describe('Create Control Event', function () {
+
+  beforeEach(function () {
+    defaultControlEvent = new EventElements.ControlEvent('event', '1', 'goal', 1, 'cause', 1)
+  })
+
+  it('Control Event can be created if report exists', function () {
+
+    // A valid Control Event
+    let event = defaultControlEvent
+
+    // Mock ReportService findById method to return a valid report
+    spyOn(reportService, 'findById').andCallFake(function (anyReportId, callback) {
+      callback(event, null)
+    })
+
+    // Mock GenericService create to return a valid new ControlEvent
+    spyOn(genericService, 'create').andCallFake(function (anyControlEvent, callback) {
+      callback(event, null)
+    })
+
+    // Create a new ControlEvent
+    eventService.createControl(event.reportId, event.type, event.matchTime, event.text,
+      event.timestamp, (createdEvent, err) => {
+      expect(createdEvent).toEqual(event)
+      expect(createdEvent).not.toBe(null)
+      expect(err).toBe(null)
+    })
+
+    // Check if genericService create method was called
+    expect(genericService.create).toHaveBeenCalled()
+
+  })
+
+  it('Control Event cannot be created if report doesnt exists', function () {
+
+    // A ControlEvent with non existent report
+    let event = defaultControlEvent
+
+    // An error to show not found report
+    let notFoundError = new InstanceNotFoundException('Non existent report', 'event.reportId', event.reportId)
+
+    // Mock ReportService findById method to return non existent report error
+    spyOn(reportService, 'findById').andCallFake(function (anyReportId, callback) {
+      callback(null, notFoundError)
+    })
+
+    // Mock GenericService create without return value (Just to check if it wasn't called)
+    spyOn(genericService, 'create')
+
+    // Create a new ControlEvent
+    eventService.createControl(event.reportId, event.type, event.matchTime, event.text,
+      event.timestamp, (createdEvent, err) => {
+      expect(createdEvent).toBe(null)
+      expect(err).not.toBe(null)
+      expect(err).toEqual(notFoundError)
+    })
+
+    // Check if genericService create method was called
+    expect(genericService.create).not.toHaveBeenCalled()
+
+  })
+
+  it('Report status should be modified when creating an EndMatchEvent and report exists', function () {
+
+      // A valid EndMatchEvent
+      let event = defaultControlEvent
+      event.type = 'end-match'
+
+      // A valid Report
+      let validReport = defaultReport
+
+      // Mock ReportService findById method to return a valid report
+      spyOn(reportService, 'findById').andCallFake(function (anyReportId, callback) {
+        callback(event, null)
+      })
+
+      // Mock GenericService create to return a valid new ControlEvent
+      spyOn(genericService, 'create').andCallFake(function (anyControlEvent, callback) {
+        callback(event, null)
+      })
+
+      // Mock ReportService update to modify report status
+      spyOn(reportService, 'update').andCallFake(function (anyReportId, reportDate,
+        reportFinished, reportLocation, reportLocal, reportVisitor, reportIncidences, callback) {
+        callback(validReport, null)
+      })
+
+      // Create a new ControlEvent
+      eventService.createControl(event.reportId, event.type, event.matchTime, event.text,
+        event.timestamp, (createdEvent, err) => {
+        expect(createdEvent).toEqual(event)
+        expect(createdEvent).not.toBe(null)
+        expect(err).toBe(null)
+      })
+
+      // Check if genericService create method was called
+      expect(genericService.create).toHaveBeenCalled()
+
+      // Check if reportService update method was called to update Report status
+      expect(reportService.update).toHaveBeenCalled()
+
+  })
+
+})
+
+describe('Delete Event', function () {
+
+  it('Event can be deleted if this event exists', function () {
+
+    // An existent event
+    let event = defaultEvent
+
+    // Mock GenericService findById to return this event
+    spyOn(genericService, 'findById').andCallFake(function (anyEventId, callback) {
+      callback(event, null)
+    })
+
+    // Mock GenericService remove to return an OK result
+    spyOn(genericService, 'remove').andCallFake(function (anyEvent, callback) {
+      callback(jasmine.any(Object), null)
+    })
+
+    // Delete this event
+    eventService.deleteEvent(event._id, (result, err) => {
+      expect(result).not.toBe(null)
+      expect(err).toBe(null)
+    })
+
+    // Check if genericService remove method was called
+    expect(genericService.remove).toHaveBeenCalled()
+
+  })
+
+  it('Event cannot be deleted if this event doesnt exists', function () {
+
+    // An non existent event
+    let event = defaultEvent
+
+    // An error to show not found event
+    let notFoundError = new InstanceNotFoundException('Non existent event', 'eventId', event._id)
+
+    // Mock GenericService findById to return a not found error
+    spyOn(genericService, 'findById').andCallFake(function (anyEventId, callback) {
+      callback(null, notFoundError)
+    })
+
+    // Mock GenericService remove (just to check if it wasn't called)
+    spyOn(genericService, 'remove')
+
+    // Delete this event
+    eventService.deleteEvent(event._id, (result, err) => {
+      expect(err).not.toBe(null)
+      expect(err).toEqual(notFoundError)
+      expect(result).toBe(null)
+    })
+
+    // Check if genericService remove method wasn't called
+    expect(genericService.remove).not.toHaveBeenCalled()
+
+  })
+
+  it('Report status should be modified when removing a EndMatchEvent which exists', function () {
+
+    // An existent EndMatchEvent
+    let event = defaultEvent
+    event.type = 'end-match'
+
+    // A valid Report
+    let validReport = defaultReport
+
+    // Mock GenericService findById to return this event
+    spyOn(genericService, 'findById').andCallFake(function (anyEventId, callback) {
+      callback(event, null)
+    })
+
+    // Mock GenericService remove to return an OK result
+    spyOn(genericService, 'remove').andCallFake(function (anyEvent, callback) {
+      callback(jasmine.any(Object), null)
+    })
+
+    // Mock ReportService update to modify report status
+    spyOn(reportService, 'update').andCallFake(function (anyReportId, reportDate,
+      reportFinished, reportLocation, reportLocal, reportVisitor, reportIncidences, callback) {
+      callback(validReport, null)
+    })
+
+    // Delete this event
+    eventService.deleteEvent(event._id, (result, err) => {
+      expect(result).not.toBe(null)
+      expect(err).toBe(null)
+    })
+
+    // Check if genericService remove method was called
+    expect(genericService.remove).toHaveBeenCalled()
+
+    // Check if reportService update method was called to update Report status
+    expect(reportService.update).toHaveBeenCalled()
+
+  })
+
 })
