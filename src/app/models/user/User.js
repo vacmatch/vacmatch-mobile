fbfd2b2ea226bc0a823935e44d6c64b0fa1d785a
Pablo Castro <pablocastro@vacmatch.com>
2016-03-08 01:55:04 +0100

    Add checks in signup method from AuthService and remove bussiness logic from AuthStore

--- src/app/models/user/User.js
+++ src/app/models/user/User.js
@@ -1,11 +1,19 @@
+import RefereeUserType from '../web/user/RefereeUserType'
 
 class User {
 
-  constructor (id, type, username, password, userId) {
+  constructor (id, type, username, password, avatarUrl, email, firstName, lastName, cardId, signKey) {
     this.id_ = id
     this.databaseType = type ? type : 'user'
-    this.username = username ? username : ''
-    this.password = password ? password : ''
+    this.username = username
+    this.password = password
+    this.avatarUrl = avatarUrl
+    this.email = email
+    this.firstName = firstName
+    this.lastName = lastName
+    this.cardId = cardId
+    this.signKey = signKey
+    this.userType = RefereeUserType.type
   }
 
 }
